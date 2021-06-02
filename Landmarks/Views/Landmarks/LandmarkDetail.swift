21078bb2e1d51468fc667cf1207ed219560277cd
shinji.nagasaki <shinji.nagasaki@infcurion.com>
2021-06-03 00:33:54 +0900

    Handling User Input

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
 
+    var landmarkIndex: Int{
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
     var body: some View {
         ScrollView{
             MapView(coordinate: landmark.locationCoordinate)
@@ -19,8 +23,13 @@ struct LandmarkDetail: View {
                 .offset(y: -120)
                 .padding(.bottom, -120)
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack{
+                    Text(landmark.name)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -45,7 +54,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[1])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
+            .environmentObject(modelData)
     }
 }
